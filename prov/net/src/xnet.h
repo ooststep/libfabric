ooststep/libfabric

commit 3ae11c498f350e3532d0c6268ca217903b3e5f0e

prov/net: Create union for msg headers
Define an explicit union for message headers and replace
embedded unions defined in structures.  This will allow
passing the union to match calls.

Signed-off-by: Sean Hefty <sean.hefty@intel.com>

prov/net/src/xnet.h
@@ -138,14 +138,16 @@ void xnet_connect_done(struct xnet_ep *ep);
 void xnet_req_done(struct xnet_ep *ep);
 int xnet_send_cm_msg(struct xnet_ep *ep);
 
+union xnet_hdrs {
+	struct xnet_base_hdr	base_hdr;
+	struct xnet_cq_data_hdr cq_data_hdr;
+	struct xnet_tag_data_hdr tag_data_hdr;
+	struct xnet_tag_hdr	tag_hdr;
+	uint8_t			max_hdr[XNET_MAX_HDR];
+};
+
 struct xnet_active_rx {
-	union {
-		struct xnet_base_hdr	base_hdr;
-		struct xnet_cq_data_hdr cq_data_hdr;
-		struct xnet_tag_data_hdr tag_data_hdr;
-		struct xnet_tag_hdr	tag_hdr;
-		uint8_t			max_hdr[XNET_MAX_HDR];
-	} hdr;
+	union xnet_hdrs		hdr;
 	size_t			hdr_len;
 	size_t			hdr_done;
 	size_t			data_left;
@@ -364,13 +366,7 @@ static inline void xnet_signal_progress(struct xnet_progress *progress)
 
 struct xnet_xfer_entry {
 	struct slist_entry	entry;
-	union {
-		struct xnet_base_hdr	base_hdr;
-		struct xnet_cq_data_hdr cq_data_hdr;
-		struct xnet_tag_data_hdr tag_data_hdr;
-		struct xnet_tag_hdr	tag_hdr;
-		uint8_t		       	max_hdr[XNET_MAX_HDR + XNET_MAX_INJECT];
-	} hdr;
+	union xnet_hdrs		hdr;
 	void			*user_buf;
 	size_t			iov_cnt;
 	struct iovec		iov[XNET_IOV_LIMIT+1];

commit 44e642206f9337444d6f0d7d63e93ea8251b144a

prov/net: Fix inject support
Commit 3ae11c498f350e3532d0c6268ca217903b3e5f0e broke inject support
by removing the buffering provided for inject.  Add the inject buffer
size back into the hdr size.

prov/net/src/xnet.h
@@ -138,12 +138,13 @@ void xnet_connect_done(struct xnet_ep *ep);
 void xnet_req_done(struct xnet_ep *ep);
 int xnet_send_cm_msg(struct xnet_ep *ep);
 
+/* Inject buffer space is included */
 union xnet_hdrs {
 	struct xnet_base_hdr	base_hdr;
 	struct xnet_cq_data_hdr cq_data_hdr;
 	struct xnet_tag_data_hdr tag_data_hdr;
 	struct xnet_tag_hdr	tag_hdr;
-	uint8_t			max_hdr[XNET_MAX_HDR];
+	uint8_t			max_hdr[XNET_MAX_HDR + XNET_MAX_INJECT];
 };
 
 struct xnet_active_rx {